811b6d2124ea2441f03376e8428a9aa1d85875c4
EDEN AJIBADE <eajibade@EDENs-MacBook-Pro.local>
2019-09-05 12:23:58 +0100

    First commit

---
+++ src/main/java/com/springbatch/demo/SpringBatchDemoApplication.java
@@ -0,0 +1,21 @@
+package com.springbatch.demo;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@SpringBootApplication
+public class SpringBatchDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchDemoApplication.class, args);
+    }
+
+
+}
